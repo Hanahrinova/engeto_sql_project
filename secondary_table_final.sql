8daa72239009fe1689db1f45340cf657425ca5a2
Hanahrinova <156844105+Hanahrinova@users.noreply.github.com>
2024-02-27 16:51:18 +0100

    Create secondary_table_final.sql

---
+++ secondary_table_final.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE t_Hana_Hrinova_project_sql_secondary_final AS
+    SELECT 
+    	e.country,
+    	e.year,
+    	e.GDP,
+    	thhpspf.payroll,
+    	thhpspf.price
+	FROM economies e
+	JOIN t_hana_hrinova_project_sql_primary_final thhpspf 
+		ON e.YEAR = thhpspf.payroll_year
+	WHERE e.country = 'Czech Republic'
+	ORDER BY e.country, e.YEAR;
