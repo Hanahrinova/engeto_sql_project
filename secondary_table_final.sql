031568fe1a60d0ec59731759f88dc22cb24a96a9
Hanahrinova <156844105+Hanahrinova@users.noreply.github.com>
2024-02-27 16:52:03 +0100

    Update secondary_table_final.sql

--- secondary_table_final.sql
+++ secondary_table_final.sql
@@ -9,4 +9,4 @@ CREATE OR REPLACE TABLE t_Hana_Hrinova_project_sql_secondary_final AS
 	JOIN t_hana_hrinova_project_sql_primary_final thhpspf 
 		ON e.YEAR = thhpspf.payroll_year
 	WHERE e.country = 'Czech Republic'
-	ORDER BY e.country, e.YEAR;
+	ORDER BY e.country, e.year;
